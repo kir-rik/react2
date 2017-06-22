7046ac22ee47aa5fce7edadbbf9fcfa61f988a5f
k.ponomarenko <k.ponomarenko@PONOMARENKO>
2017-06-22 19:22:27 +0400

    css

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <div className="Content">
-          <StaticTable data={DataProvider.getStaticTableData()} className='Table-1'/>
+        <div className="content">
+          <StaticTable data={DataProvider.getStaticTableData()} className='table-1'/>
         </div>
       </div>
     );
